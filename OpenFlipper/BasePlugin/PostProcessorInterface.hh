2095c4c311e3c06f1059abf6d84ef9642cbad88a
Jan Möbius <moebius@cs.rwth-aachen.de>
2015-11-16 15:47:41 +0100

    Uninitialized variable in postrocessor interface

--- OpenFlipper/BasePlugin/PostProcessorInterface.hh
+++ OpenFlipper/BasePlugin/PostProcessorInterface.hh
@@ -125,7 +125,7 @@ struct DLLEXPORT PostProcessorOutput
 // in/out format descriptor
 struct PostProcessorFormatDesc
 {
-  PostProcessorFormatDesc(bool _multisampled = false) : supportMultisampling_(_multisampled)
+  PostProcessorFormatDesc(bool _multisampled = false) : supportMultisampling_(_multisampled),outputFormat_(PostProcessorFormat_DONTCARE)
   {}
 
 

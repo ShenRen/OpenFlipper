bf2a51c577a6e5856b612c29e510b18cbe0ebb43
Christopher Tenter <christopher.tenter@rwth-aachen.de>
2016-03-31 15:13:29 +0200

    query attribute information from glsl compiler in driver

--- ACG/GL/ShaderCache.cc
+++ ACG/GL/ShaderCache.cc
@@ -663,7 +663,7 @@ std::vector<ShaderCache::VertexShaderAttributeInfo>& ShaderCache::getVertexShade
 
 
   new style:  (opengl 4.3+)
-  glGetProgramInterfaceiv(program, GL_PROGRAM_INPUT, GL_ACTIVE_RESOURCES?, &n);
+  glGetProgramInterfaceiv(program, GL_PROGRAM_INPUT, GL_ACTIVE_RESOURCES, &n);
 
   for i = 0..n
     const GLenum props[3] = {GL_NAME_LENGTH, GL_TYPE, GL_ARRAY_SIZE};
@@ -678,202 +678,295 @@ std::vector<ShaderCache::VertexShaderAttributeInfo>& ShaderCache::getVertexShade
   // todo: generate dummy fragment shader, link and query information
 
 
-  std::ifstream file;
+  std::ifstream file(_vertexShaderFile, std::ios_base::in);
 
-  file.open(_vertexShaderFile, std::ios_base::in);
 
   if (file.is_open())
   {
-    bool tokenize = false;
-    if (tokenize)
+    QString shaderSource;
+
+    if (fileInfo.size())
     {
+      std::vector<char> fileData(fileInfo.size(), 0);
+      file.read(&fileData[0], fileInfo.size());
 
-      QString shaderSource;
+      shaderSource = &fileData[0];
+    }
 
-      if (fileInfo.size())
+    file.seekg(std::ios_base::beg);
+
+    bool useGL = false;
+
+    if (useGL)
+    {
+      // compile shader and query information from the driver
+
+      // dummy fragment shader
+      const QString dummyShader = QString("#version 130\nout vec4 __dummy_Color__;\nvoid main(){__dummy_Color__ = vec4(1,1,1,1);}");
+
+      GLSL::FragmentShader* fragShader = new GLSL::FragmentShader();
+      GLSL::VertexShader* vertShader = new GLSL::VertexShader();
+
+      vertShader->setSource(shaderSource.split('\n'));
+      fragShader->setSource(dummyShader.split('\n'));
+
+      vertShader->compile();
+      fragShader->compile();
+
+      GLSL::Program* prog = new GLSL::Program();
+      prog->attach(vertShader);
+      prog->attach(fragShader);
+
+      prog->link();
+      glCheckErrors();
+
+      if (prog->isLinked())
       {
-        std::vector<char> fileData(fileInfo.size(), 0);
-        file.read(&fileData[0], fileInfo.size());
+        bool oldStyle = !ACG::openGLVersion(4, 3);
 
-        shaderSource = &fileData[0];
-      }
-
-//      shaderSource = shaderSource.simplified();
-
-      // remove comments
-
-      // single line comments
-      for (int i = 0; i < shaderSource.length() - 1; ++i)
-      {
-        if (shaderSource[i] == '/' && shaderSource[i + 1] == '/')
+        if (oldStyle)
         {
-          int commentEnd = shaderSource.indexOf('\n', i);
+          // get number of attributes
+          int n = 0;
+          glGetProgramiv(prog->getProgramId(), GL_ACTIVE_ATTRIBUTES, &n);
 
-          if (commentEnd < 0)
-            commentEnd = shaderSource.length() - 1;
-
-          shaderSource.remove(i, commentEnd - i);
-          --i; // check again at same position
-        }
-      }
-
-      // multi line comments
-      for (int i = 0; i < shaderSource.length() - 1; ++i)
-      {
-        if (shaderSource[i] == '/' && shaderSource[i + 1] == '*')
-        {
-          // find comment end
-          int commentEnd = -1;
-
-          for (int k = i + 2; k < shaderSource.length() - 1; ++k)
+          // get attribute info
+          for (int i = 0; i < n; ++i)
           {
-            if (shaderSource[k] == '*' && shaderSource[k + 1] == '/')
-              commentEnd = k + 1;
+            char name[0xff];
+            int nameLen = 0;
+            int size = 0;
+            GLenum type = 0;
+
+            glGetActiveAttrib(prog->getProgramId(), i, 0xff, &nameLen, &size, &type, name);
+
+            VertexShaderAttributeInfo attribute;
+            attribute.name = name;
+            attribute.flat = false; // can't query interpolation mode from driver
+            attribute.size = size;
+            attribute.integer = false; // todo: interpret type variable
+            attribute.generated = false; // todo: check name for shadergenerator keywords
           }
-
-          if (commentEnd < 0)
-            commentEnd = shaderSource.length() - 1;
-
-          shaderSource.remove(i, commentEnd - i);
-          --i; // check again at same position
         }
+        else
+        {
+          // get number of attributes
+          int n = 0;
+          glGetProgramInterfaceiv(prog->getProgramId(), GL_PROGRAM_INPUT, GL_ACTIVE_RESOURCES, &n);
+
+          for (int i = 0; i < n; ++i)
+          {
+            const GLenum props[3] = { GL_NAME_LENGTH, GL_TYPE, GL_ARRAY_SIZE };
+            GLint values[3];
+            char name[0xff];
+            glGetProgramResourceiv(prog->getProgramId(), GL_PROGRAM_INPUT, i, 3, props, 3, 0, values);
+            glGetProgramResourceName(prog->getProgramId(), GL_PROGRAM_INPUT, i, props[0], 0, name);
+
+            VertexShaderAttributeInfo attribute;
+            attribute.name = name;
+            attribute.flat = false; // can't query interpolation mode from driver
+            attribute.size = values[2];
+            attribute.integer = false; // todo: interpret type variable
+            attribute.generated = false; // todo: check name for shadergenerator keywords
+          }
+        }
+
       }
 
-      
-
-      shaderSource.replace('\t', ' ');
-      shaderSource.replace('\n', ' ');
-      shaderSource = shaderSource.simplified();
-
-      // tokenize
-      QRegExp separators("(\\ |\\,|\\.|\\:|\\}|\\{|\\)|\\(|\\;");
-      QStringList tokens = shaderSource.split(separators);
-
+      delete prog;
+      delete fragShader;
+      delete vertShader;
     }
     else
     {
-      while (!file.eof())
+      // manually scan shader source
+
+      bool tokenize = false;
+      if (tokenize)
       {
-        char line[0xff];
-        file.getline(line, 0xff);
 
-        // todo:
-        // - identify and remove multi line comments from file
-        // - check if qt can tokenize the text file for more robust scanning
+        
 
-        // get rid of whitespaces at begin/end, and internal padding
-        QString strLine = line;
-        strLine = strLine.simplified();
+        //      shaderSource = shaderSource.simplified();
 
-        // pattern matching for vertex input attributes
-        if (!strLine.startsWith("//") && !strLine.startsWith("*/"))
+        // remove comments
+
+        // single line comments
+        for (int i = 0; i < shaderSource.length() - 1; ++i)
         {
-
-          if (strLine.startsWith("in ") || strLine.contains(" in "))
+          if (shaderSource[i] == '/' && shaderSource[i + 1] == '/')
           {
-            // extract 
-            int semIdx = strLine.indexOf(';');
+            int commentEnd = shaderSource.indexOf('\n', i);
 
+            if (commentEnd < 0)
+              commentEnd = shaderSource.length() - 1;
 
-            if (semIdx >= 0)
+            shaderSource.remove(i, commentEnd - i);
+            --i; // check again at same position
+          }
+        }
+
+        // multi line comments
+        for (int i = 0; i < shaderSource.length() - 1; ++i)
+        {
+          if (shaderSource[i] == '/' && shaderSource[i + 1] == '*')
+          {
+            // find comment end
+            int commentEnd = -1;
+
+            for (int k = i + 2; k < shaderSource.length() - 1; ++k)
             {
-              // property name = string before semicolon without whitespace
+              if (shaderSource[k] == '*' && shaderSource[k + 1] == '/')
+                commentEnd = k + 1;
+            }
 
-              // remove parts after semicolon
-              QString strName = strLine;
-              strName.remove(semIdx, strName.length());
+            if (commentEnd < 0)
+              commentEnd = shaderSource.length() - 1;
 
-              strName = strName.simplified();
+            shaderSource.remove(i, commentEnd - i);
+            --i; // check again at same position
+          }
+        }
 
-              // property name = string between last whitespace and last character
-              int lastWhite = strName.lastIndexOf(' ');
 
-              if (lastWhite >= 0 && !strName.contains(")"))
+
+        shaderSource.replace('\t', ' ');
+        shaderSource.replace('\n', ' ');
+        shaderSource = shaderSource.simplified();
+
+        // tokenize
+        QRegExp separators("(\\ |\\,|\\.|\\:|\\}|\\{|\\)|\\(|\\;");
+        QStringList tokens = shaderSource.split(separators);
+
+        // todo: analyze tokens..
+      }
+      else
+      {
+        while (!file.eof())
+        {
+          char line[0xff];
+          file.getline(line, 0xff);
+
+          // todo:
+          // - identify and remove multi line comments from file
+          // - check if qt can tokenize the text file for more robust scanning
+
+          // get rid of whitespaces at begin/end, and internal padding
+          QString strLine = line;
+          strLine = strLine.simplified();
+
+          // pattern matching for vertex input attributes
+          if (!strLine.startsWith("//") && !strLine.startsWith("*/"))
+          {
+
+            if (strLine.startsWith("in ") || strLine.contains(" in "))
+            {
+              // extract 
+              int semIdx = strLine.indexOf(';');
+
+
+              if (semIdx >= 0)
               {
-                strName.remove(0, lastWhite);
+                // property name = string before semicolon without whitespace
+
+                // remove parts after semicolon
+                QString strName = strLine;
+                strName.remove(semIdx, strName.length());
 
                 strName = strName.simplified();
 
-                VertexShaderAttributeInfo attribute;
-                attribute.name = strName.toStdString();
+                // property name = string between last whitespace and last character
+                int lastWhite = strName.lastIndexOf(' ');
 
-                // check for reserved input attributes
-                if (strName != "inPosition" ||
-                  strName != "inTexCoord" ||
-                  strName != "inNormal" ||
-                  strName != "inColor")
-                  attribute.generated = true;
-                else
-                  attribute.generated = false;
-
-
-                if (strLine.contains("flat "))
-                  attribute.flat = true;
-
-
-                // analyze type
-
-                attribute.integer = false;
-                attribute.size = 0;
-
-                if (strLine.indexOf("float ") >= 0 ||
-                  strLine.indexOf("half ") >= 0)
-                  attribute.size = 1;
-                else if (strLine.indexOf("int ") >= 0 ||
-                  strLine.indexOf("uint ") >= 0 ||
-                  strLine.indexOf("short ") >= 0 ||
-                  strLine.indexOf("ushort ") >= 0)
+                if (lastWhite >= 0 && !strName.contains(")"))
                 {
-                  attribute.size = 1;
-                  attribute.integer = true;
-                }
-                else
-                {
-                  // vector attribute
-                  int vecStartIndex = strLine.indexOf("vec");
+                  strName.remove(0, lastWhite);
 
-                  // get vector size
-                  if (vecStartIndex >= 0 &&
-                    vecStartIndex + 3 < strLine.length())
+                  strName = strName.simplified();
+
+                  VertexShaderAttributeInfo attribute;
+                  attribute.name = strName.toStdString();
+
+                  // check for reserved input attributes
+                  if (strName != "inPosition" ||
+                    strName != "inTexCoord" ||
+                    strName != "inNormal" ||
+                    strName != "inColor")
+                    attribute.generated = true;
+                  else
+                    attribute.generated = false;
+
+
+                  if (strLine.contains("flat "))
+                    attribute.flat = true;
+
+
+                  // analyze type
+
+                  attribute.integer = false;
+                  attribute.size = 0;
+
+                  if (strLine.indexOf("float ") >= 0 ||
+                    strLine.indexOf("half ") >= 0)
+                    attribute.size = 1;
+                  else if (strLine.indexOf("int ") >= 0 ||
+                    strLine.indexOf("uint ") >= 0 ||
+                    strLine.indexOf("short ") >= 0 ||
+                    strLine.indexOf("ushort ") >= 0)
                   {
-                    QChar sizeChar = strLine[vecStartIndex + 3];
+                    attribute.size = 1;
+                    attribute.integer = true;
+                  }
+                  else
+                  {
+                    // vector attribute
+                    int vecStartIndex = strLine.indexOf("vec");
 
-                    if (sizeChar.isDigit())
+                    // get vector size
+                    if (vecStartIndex >= 0 &&
+                      vecStartIndex + 3 < strLine.length())
                     {
-                      int vecSize = sizeChar.digitValue();
+                      QChar sizeChar = strLine[vecStartIndex + 3];
 
-                      if (vecSize > 1 && vecSize < 5)
+                      if (sizeChar.isDigit())
                       {
-                        attribute.size = vecSize;
+                        int vecSize = sizeChar.digitValue();
 
-                        // now check vector data type
-
-                        if (vecStartIndex + 4 < strLine.length())
+                        if (vecSize > 1 && vecSize < 5)
                         {
-                          QChar typeChar = strLine[vecStartIndex + 4];
+                          attribute.size = vecSize;
 
-                          if (typeChar == QChar(' '))
-                            attribute.integer = false;
-                          else if (typeChar == QChar('i') ||
-                            typeChar == QChar('u'))
-                            attribute.integer = true;
-                          else
-                            std::cerr << "warning - ACG::ShaderCache - vector type analysis failed" << std::endl;
+                          // now check vector data type
+
+                          if (vecStartIndex + 4 < strLine.length())
+                          {
+                            QChar typeChar = strLine[vecStartIndex + 4];
+
+                            if (typeChar == QChar(' '))
+                              attribute.integer = false;
+                            else if (typeChar == QChar('i') ||
+                              typeChar == QChar('u'))
+                              attribute.integer = true;
+                            else
+                              std::cerr << "warning - ACG::ShaderCache - vector type analysis failed" << std::endl;
+                          }
                         }
                       }
                     }
                   }
-                }
 
-                // save attribute
-                data.attributes.push_back(attribute);
+                  // save attribute
+                  data.attributes.push_back(attribute);
+                }
               }
             }
           }
         }
       }
     }
+
   }
+  
 
   if (entry == cacheAttributes_.end())
     entry = cacheAttributes_.insert(key, data);

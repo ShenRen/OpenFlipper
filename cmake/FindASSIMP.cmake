4bcf205053392e67ebaf57bc2dcc21006f6b3ae1
Jan Möbius <moebius@cs.rwth-aachen.de>
2016-04-26 15:28:30 +0200

    Add cmake flag to set library dir for search path base, added vs2015 search paths

--- cmake/FindASSIMP.cmake
+++ cmake/FindASSIMP.cmake
@@ -25,17 +25,25 @@ if( WIN32 )
    SET( DIRSUFFIX "x86" )
  endif()
 
-  if ( CMAKE_GENERATOR MATCHES "^Visual Studio 10.*" )
-    SET(VS_SEARCH_PATH "c:/libs/vs2010/x32/")
-  elseif ( CMAKE_GENERATOR MATCHES "^Visual Studio 11.*Win64" )
-    SET(VS_SEARCH_PATH "c:/libs/vs2012/x64/")
-  elseif ( CMAKE_GENERATOR MATCHES "^Visual Studio 11.*" )
-    SET(VS_SEARCH_PATH "c:/libs/vs2012/x32/")
-  elseif ( CMAKE_GENERATOR MATCHES "^Visual Studio 12.*Win64" )
-    SET(VS_SEARCH_PATH "c:/libs/vs2013/x64/")
-  elseif ( CMAKE_GENERATOR MATCHES "^Visual Studio 12.*" )
-    SET(VS_SEARCH_PATH "c:/libs/vs2013/x32/")
-  endif()
+ # Check if the base path is set
+ if ( NOT CMAKE_WINDOWS_LIBS_DIR )
+   # This is the base directory for windows library search used in the finders we shipp.
+   set(CMAKE_WINDOWS_LIBS_DIR "c:\libs" CACHE STRING "Default Library search dir on windows." )
+ endif
+
+ if ( CMAKE_GENERATOR MATCHES "^Visual Studio 11.*Win64" )
+   SET(VS_SEARCH_PATH "${CMAKE_WINDOWS_LIBS_DIR}/vs2012/x64/")
+ elseif ( CMAKE_GENERATOR MATCHES "^Visual Studio 11.*" )
+   SET(VS_SEARCH_PATH "${CMAKE_WINDOWS_LIBS_DIR}/vs2012/x32/")
+ elseif ( CMAKE_GENERATOR MATCHES "^Visual Studio 12.*Win64" )
+   SET(VS_SEARCH_PATH "${CMAKE_WINDOWS_LIBS_DIR}/vs2013/x64/")
+ elseif ( CMAKE_GENERATOR MATCHES "^Visual Studio 12.*" )
+   SET(VS_SEARCH_PATH "${CMAKE_WINDOWS_LIBS_DIR}/vs2013/x32/")
+ elseif ( CMAKE_GENERATOR MATCHES "^Visual Studio 14.*Win64" )
+   SET(VS_SEARCH_PATH "${CMAKE_WINDOWS_LIBS_DIR}/vs2015/x64/")
+ elseif ( CMAKE_GENERATOR MATCHES "^Visual Studio 14.*" )
+   SET(VS_SEARCH_PATH "${CMAKE_WINDOWS_LIBS_DIR}/vs2015/x32/")
+ endif()
 
  # Find the headers
  FIND_PATH( ASSIMP_INCLUDE_DIR assimp/scene.h
@@ -48,7 +56,7 @@ if( WIN32 )
  FIND_LIBRARY( ASSIMP_LIBRARY
                NAMES assimp
 	       PATH_SUFFIXES ${DIRSUFFIX}
-               PATHS "C:/libs/assimp/lib"
+               PATHS "${CMAKE_WINDOWS_LIBS_DIR}/assimp/lib"
                      "${VS_SEARCH_PATH}assimp/lib" 
 	             "C:/Program Files/Assimp/lib"
                )  

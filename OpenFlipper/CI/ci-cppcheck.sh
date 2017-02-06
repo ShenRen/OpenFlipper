5ab0aa1912466d4aba76cc337dcba350be9fb2ae
Jan Möbius <moebius@cs.rwth-aachen.de>
2017-02-06 19:00:12 +0100

    Lower allowed cppcheck error count

--- OpenFlipper/CI/ci-cppcheck.sh
+++ OpenFlipper/CI/ci-cppcheck.sh
@@ -59,7 +59,7 @@ echo "CPPCHECK Summary"
 echo "=============================================================================="
 echo -e "${NC}"
 
-MAX_ERROR=218
+MAX_ERROR=203
 
 if [ $COUNT -gt $MAX_ERROR ]; then
   echo -e ${WARNING}

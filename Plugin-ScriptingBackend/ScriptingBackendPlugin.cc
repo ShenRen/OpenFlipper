4a71401ed339277cd98d6d13f9924e1fb44e28c8
Jascha Wedowski <jascha.wedowski@rwth-aachen.de>
2017-06-12 18:20:51 +0200

    started work on qjsengine based scripting backend plugin

---
+++ Plugin-ScriptingBackend/ScriptingBackendPlugin.cc
@@ -0,0 +1,82 @@
+/*===========================================================================*\
+*                                                                            *
+*                              OpenFlipper                                   *
+ *           Copyright (c) 2001-2015, RWTH-Aachen University                 *
+ *           Department of Computer Graphics and Multimedia                  *
+ *                          All rights reserved.                             *
+ *                            www.openflipper.org                            *
+ *                                                                           *
+ *---------------------------------------------------------------------------*
+ * This file is part of OpenFlipper.                                         *
+ *---------------------------------------------------------------------------*
+ *                                                                           *
+ * Redistribution and use in source and binary forms, with or without        *
+ * modification, are permitted provided that the following conditions        *
+ * are met:                                                                  *
+ *                                                                           *
+ * 1. Redistributions of source code must retain the above copyright notice, *
+ *    this list of conditions and the following disclaimer.                  *
+ *                                                                           *
+ * 2. Redistributions in binary form must reproduce the above copyright      *
+ *    notice, this list of conditions and the following disclaimer in the    *
+ *    documentation and/or other materials provided with the distribution.   *
+ *                                                                           *
+ * 3. Neither the name of the copyright holder nor the names of its          *
+ *    contributors may be used to endorse or promote products derived from   *
+ *    this software without specific prior written permission.               *
+ *                                                                           *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS       *
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED *
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A           *
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER *
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,  *
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,       *
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR        *
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF    *
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING      *
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS        *
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.              *
+*                                                                            *
+\*===========================================================================*/
+
+/*===========================================================================*\
+*                                                                            *
+*   $Revision$                                                       *
+*   $LastChangedBy$                                                *
+*   $Date$                     *
+*                                                                            *
+\*===========================================================================*/
+
+#include "ScriptingBackendPlugin.hh"
+
+#include "OpenFlipper/common/GlobalOptions.hh"
+
+#if QT_VERSION >= 0x050000
+#else
+#include <QtGui>
+#endif
+
+#include <QJSValue>
+
+
+ScriptingBackend::ScriptingBackend()
+{
+
+}
+
+void ScriptingBackend::pluginsInitialized()
+{
+    // TODO: Replace with std::make_unique when we start to support c++14
+    jsEngine_.reset(new QJSEngine());
+
+    QJSValue self = jsEngine_->newQObject(this);
+    jsEngine_->globalObject().setProperty("scriptingBackend", self);
+
+    QJSValue res = jsEngine_->evaluate("scriptingBackend.log(\"Hello\")");
+}
+
+
+#if QT_VERSION < 0x050000
+  Q_EXPORT_PLUGIN2( skriptingbackendplugin , ScriptingBackendPlugin );
+#endif
+
---
+++ Plugin-ScriptingBackend/ScriptingBackendPlugin.hh
@@ -0,0 +1,107 @@
+/*===========================================================================*\
+*                                                                            *
+*                              OpenFlipper                                   *
+ *           Copyright (c) 2001-2015, RWTH-Aachen University                 *
+ *           Department of Computer Graphics and Multimedia                  *
+ *                          All rights reserved.                             *
+ *                            www.openflipper.org                            *
+ *                                                                           *
+ *---------------------------------------------------------------------------*
+ * This file is part of OpenFlipper.                                         *
+ *---------------------------------------------------------------------------*
+ *                                                                           *
+ * Redistribution and use in source and binary forms, with or without        *
+ * modification, are permitted provided that the following conditions        *
+ * are met:                                                                  *
+ *                                                                           *
+ * 1. Redistributions of source code must retain the above copyright notice, *
+ *    this list of conditions and the following disclaimer.                  *
+ *                                                                           *
+ * 2. Redistributions in binary form must reproduce the above copyright      *
+ *    notice, this list of conditions and the following disclaimer in the    *
+ *    documentation and/or other materials provided with the distribution.   *
+ *                                                                           *
+ * 3. Neither the name of the copyright holder nor the names of its          *
+ *    contributors may be used to endorse or promote products derived from   *
+ *    this software without specific prior written permission.               *
+ *                                                                           *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS       *
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED *
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A           *
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER *
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,  *
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,       *
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR        *
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF    *
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING      *
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS        *
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.              *
+*                                                                            *
+\*===========================================================================*/
+
+/*===========================================================================*\
+*                                                                            *
+*   $Revision$                                                       *
+*   $LastChangedBy$                                                *
+*   $Date$                     *
+*                                                                            *
+\*===========================================================================*/
+
+#ifndef SCRIPTINGPLUGIN_HH
+#define SCRIPTINGPLUGIN_HH
+
+#include <QObject>
+#include <QMenuBar>
+
+#include <OpenFlipper/common/Types.hh>
+#include <OpenFlipper/BasePlugin/BaseInterface.hh>
+#include <OpenFlipper/BasePlugin/MenuInterface.hh>
+#include <OpenFlipper/BasePlugin/ScriptInterface.hh>
+#include <OpenFlipper/BasePlugin/RPCInterface.hh>
+#include <OpenFlipper/BasePlugin/LoggingInterface.hh>
+
+#include <memory>
+#include <QJSEngine>
+
+class ScriptingBackend : public QObject, BaseInterface, MenuInterface, ScriptInterface, RPCInterface, LoggingInterface
+{
+Q_OBJECT
+Q_INTERFACES(BaseInterface)
+Q_INTERFACES(MenuInterface)
+Q_INTERFACES(ScriptInterface)
+Q_INTERFACES(RPCInterface)
+Q_INTERFACES(LoggingInterface)
+
+#if QT_VERSION >= 0x050000
+  Q_PLUGIN_METADATA(IID "org.OpenFlipper.Plugins.Plugin-Scripting")
+#endif
+
+    std::unique_ptr<QJSEngine> jsEngine_;
+
+public:
+signals:
+
+    void log(Logtype _type, QString _message);
+    void log(QString _message);
+
+private slots:
+
+
+  void pluginsInitialized();
+
+  void noguiSupported( ) {} ;
+
+public :
+
+  ScriptingBackend();
+
+  QString name() { return (QString(tr("Scripting Backend"))); };
+  QString description( ) { return (QString(tr("Provides Updated Scripting for OpenFlipper"))); };
+
+
+public slots:
+  QString version() { return QString("1.0"); };
+};
+
+#endif //SCRIPTINGPLUGIN_HH
+

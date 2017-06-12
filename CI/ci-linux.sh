631644e191ebbcdb29181caf1c8c287ed1ca4fd4
Jan Möbius <moebius@cs.rwth-aachen.de>
2017-06-12 16:43:04 +0200

    Added Qt 5.9 test builds for linux

--- CI/ci-linux.sh
+++ CI/ci-linux.sh
@@ -53,6 +53,10 @@ elif [ "$QTVERSION" == "QT5" ]; then
   echo "Building with QT5";
   BUILDPATH="$BUILDPATH-qt5"
   OPTIONS="$OPTIONS -DFORCE_QT4=FALSE -DQWT6_INCLUDE_DIR=~/sw/qwt-6.1.2-qt5/include -DQWT6_LIBRARY_DIR=~/sw/qwt-6.1.2-qt5/lib -DQWT6_LIBRARY=~/sw/qwt-6.1.2-qt5/lib/libqwt-qt5.so -DQT5_INSTALL_PATH=~/sw/qt-5.5.1/5.5/gcc_64"
+elif [ "$QTVERSION" == "QT5.9.0" ]; then
+  echo "Building with QT5.9.0";
+  BUILDPATH="$BUILDPATH-qt5.9.0"
+  OPTIONS="$OPTIONS -DFORCE_QT4=FALSE -DQWT6_INCLUDE_DIR=~/sw/qwt-6.1.3-qt5.9.0/include -DQWT6_LIBRARY_DIR=~/sw/qwt-6.1.3-qt5.9.0/lib -DQWT6_LIBRARY=~/sw/qwt-6.1.3-qt5.9.0/lib/libqwt.so -DQT5_INSTALL_PATH=~/sw/Qt/5.9/gcc_64"
 fi
 
 echo "Building with path: build-release-$BUILDPATH"

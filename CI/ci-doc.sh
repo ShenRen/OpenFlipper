f533993ce191666ff29902910e401a736b922f7a
Martin Schultz <Martin.Schultz@rwth-aachen.de>
2017-07-12 16:01:25 +0200

    changed ci-doc script for debugging

--- CI/ci-doc.sh
+++ CI/ci-doc.sh
@@ -10,6 +10,8 @@ cmake -DCMAKE_BUILD_TYPE=Release -DOPENFLIPPER_BUILD_UNIT_TESTS=FALSE ../
 
 make doc >> Doxygen-warning.log 2> Doxygen-error.log
 
+ls -l
+
 # Copy to webserver
-scp -r -P 2222 Build/share/OpenFlipper/Doc/DeveloperHTML/* hudson@web4-info8:OpenFlipper-Free-Daily/Doc/Free/Developer
-scp -r -P 2222 Build/share/OpenFlipper/Doc/UserHTML/* hudson@web4-info8:OpenFlipper-Free-Daily/Doc/Free/User
+scp -r -P 2222 build-release/Build/share/OpenFlipper/Doc/DeveloperHTML/* hudson@web4-info8:OpenFlipper-Free-Daily/Doc/Free/Developer
+scp -r -P 2222 build-release/Build/share/OpenFlipper/Doc/UserHTML/* hudson@web4-info8:OpenFlipper-Free-Daily/Doc/Free/User
